f80f60703a1f1a77c04c8f1c71b4b48a04e0be51
rimmmy <garimbaek@gmail.com>
2022-09-16 14:20:33 +0900

    img src 일괄수정

--- js/core.js
+++ js/core.js
@@ -192,19 +192,19 @@ function historyEvent(){
        if(matchMedia("screen and (min-width: 1280px)").matches){
         if(scroll_top > 662 && scroll_top<1640){
             $('.historyContent aside').addClass('active');
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_01.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_01.png")');
         }
         else if(scroll_top > 1640 && scroll_top<2700){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_02.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_02.png")');
         }
         else if(scroll_top > 2700 && scroll_top<3600){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_03.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_03.png")');
         }
         else if(scroll_top > 3600 && scroll_top<4500){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_04.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_04.png")');
         }
         else if(scroll_top > 4500 && scroll_top<5000){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_05.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_05.png")');
         }
         else if(scroll_top > 5000 && scroll_top<6000){
             $('.historyContent aside').removeClass('active');
@@ -213,19 +213,19 @@ function historyEvent(){
        if(matchMedia("screen and (min-width: 768px) and (max-width: 1279px)").matches){
         if(scroll_top > 662 && scroll_top<2018){
             $('.historyContent aside').addClass('active');
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_01.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_01.png")');
         }
         else if(scroll_top > 2018 && scroll_top<3630){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_02.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_02.png")');
         }
         else if(scroll_top > 3630 && scroll_top<5105){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_03.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_03.png")');
         }
         else if(scroll_top > 5105 && scroll_top<6415){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_04.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_04.png")');
         }
         else if(scroll_top > 6415 && scroll_top<7600){
-            $('.historyContent aside').css('background-image','url("../images/history_asdie_05.png")');
+            $('.historyContent aside').css('background-image','url("images/history_asdie_05.png")');
         }
         else if(scroll_top > 7600){
             $('.historyContent aside').removeClass('active');
